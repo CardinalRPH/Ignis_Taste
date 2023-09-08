b9eb2061b3bc78e816646a3f7876d25c616b56a2
CardinalRPH <rayhanfebriyan62@gmail.com>
2023-09-08 16:46:19 +0700

    Publish commit

--- src/scripts/views/templates/like_dislike_btn.js
+++
@@ -1,5 +0,0 @@
-const LikeButton = '<div id=outer-like><i aria-label="unlike this restaurant" class="fa-solid fa-heart"></i></div>';
-
-const LikedButton = '<div id=outer-like><i aria-label="like this restaurant" class="fa-regular fa-heart"></i>';
-
-export { LikeButton, LikedButton };
